fb8c9d471705193517f1169f101849375e07561e
Andrew Mauragis <andrew@mauragis.com>
2014-07-22 16:56:02 -0400

    gofmt

--- bofhwits.go
+++ bofhwits.go
@@ -1,23 +1,20 @@
 package main
 
 import (
-    "github.com/amauragis/bofhwits/bofhwitsbot"
-    "flag"
-    "fmt"
+	"flag"
+	"fmt"
+	"github.com/amauragis/bofhwits/bofhwitsbot"
 )
 
 // setup -c flag to pass a configuration file to the bot.  I suppose if you want multiple bots, you can use multiple configuration files
 var config_file = flag.String("c", "config/bofhwits.yaml", "The path to the configuration file to use (default config/bofhwits.yaml)")
 
-
 func main() {
 
 	flag.Parse()
 
-	bot := bofhwitsbot.BofhwitsBot{ConfigFilePath:*config_file}
-
+	bot := bofhwitsbot.BofhwitsBot{ConfigFilePath: *config_file}
 	bot.LoadConfig()
-
 	bot.RunBot()
 
 }
