98c27560c94c259c2357be2f8c6b1996b6ae60df
Andrew Mauragis <andrew@mauragis.com>
2014-10-16 00:02:22 -0400

    Adjusted searchHistory functionality to select the most likely entry

--- bofhwitsbot/messagebuffer.go
+++ bofhwitsbot/messagebuffer.go
@@ -8,6 +8,19 @@ import (
 
 var eventRing *ring.Ring
 
+func MinIntS(v []int) (min int, idx int) {
+	if len(v) > 0 {
+		m = v[0]
+	}
+	for i := 1; i < len(v); i++ {
+		if v[i] < m {
+			idx = i
+			m = v[i]
+		}
+	}
+	return
+}
+
 func InitRing(size int) {
 	eventRing = ring.New(size)
 }
@@ -30,18 +43,28 @@ func getHistory() []irc.Event {
 	return history
 }
 
-func searchHistory(history []irc.Event, searchStr string) (matchIndicies []int) {
+func searchHistory(history []irc.Event, searchStr string) (match int) {
 
-	costs := LevenshteinCost{1, 2, 2} // set delete, insert, and substitution costs
+	const costs = LevenshteinCost{1, 2, 2} // set delete, insert, and substitution costs
+	const timestampCostBuffer = 7
+
+	var distances [len(history)]int
+
 	for index := range history {
-		dist := Levenshtein(searchStr, "<"+history[index].Nick+"> "+history[index].Message(), &costs)
-		if dist < 6 {
-			matchIndicies = append(matchIndicies, index)
-		}
+		distances[index] = Levenshtein(searchStr, "<"+history[index].Nick+"> "+history[index].Message(), &costs)
+
 		fmt.Printf("%v: %v (%v) | %v\n", index, searchStr, "<"+history[index].Nick+"> "+history[index].Message(), dist)
 	}
+	min, minIdx = MinIntS(distances)
 
-	return matchIndicies
+	// if the match is worse than half the length + the timestamp buffer, its probably trash
+	if min > len(history[minIdx])/2+timestampCostBuffer {
+		match = -1
+	} else {
+		match = minIdx
+	}
+
+	return match
 }
 
 // func main() {
